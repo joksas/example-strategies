31c7b9a749ee4580ab59754f19bad25f93741c78
Dovydas Joksas <dovydas@yoshke.org>
2022-01-20 22:19:00 +0000

    set minimum logging level to WARNING

--- example_strategies/__init__.py
+++ example_strategies/__init__.py
@@ -6,7 +6,7 @@ from example_strategies.strategies import (MeanRevertingStrategy,
 
 logging.basicConfig(
     stream=sys.stdout,
-    level=logging.INFO,
+    level=logging.WARNING,
     format="%(asctime)s (%(levelname)s): %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )

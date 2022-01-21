eae2cbc755549eef24b30b57936b1a9b779f6ea6
Dovydas Joksas <dovydas@yoshke.org>
2022-01-21 11:46:01 +0000

    set trading amount using sizers

--- example_strategies/utils.py
+++ example_strategies/utils.py
@@ -1,11 +1,12 @@
 import backtrader as bt
 
 
-def get_cerebro(strategy: bt.Strategy, ticker_data, value, params):
+def get_cerebro(strategy: bt.Strategy, ticker_data, value, params, percent_size=90):
     cerebro = bt.Cerebro()
     cerebro.addstrategy(strategy, **params)
     data = bt.feeds.PandasData(dataname=ticker_data)
     cerebro.adddata(data)
     cerebro.broker.setcash(value)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=percent_size)
 
     return cerebro

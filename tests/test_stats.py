073b5037d75c925dfdac374e6348444c9e4bb417
Dovydas Joksas <dovydas@yoshke.org>
2022-01-23 20:51:23 +0000

    ensure sensible Hurst exponents for mean-reverting and trending series

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,10 +4,16 @@ from example_strategies import stats
 
 def test_hurst_exponent():
     np.random.seed(0)
-    # Gometric Brownian Motion, Mean-Reverting and Trending Series. Adapted
+    # Mean-reverting, gometric brownian motion and trending series. Adapted
     # from "Successful Algorithmic Trading" by Michael L. Halls-Moore.
-    gbm = np.log(np.cumsum(np.random.randn(100000)) + 1000)
     mr = np.log(np.random.randn(100000) + 1000)
+    gbm = np.log(np.cumsum(np.random.randn(100000)) + 1000)
     tr = np.log(np.cumsum(np.random.randn(100000) + 1) + 1000)
 
-    assert stats.hurst_exponent(mr) < stats.hurst_exponent(gbm) < stats.hurst_exponent(tr)
+    h_mr = stats.hurst_exponent(mr)
+    h_gbm = stats.hurst_exponent(gbm)
+    h_tr = stats.hurst_exponent(tr)
+
+    assert h_mr < h_gbm < h_tr
+    assert h_mr < 0.5
+    assert h_tr > 0.5
